eb4133f39382526907e44a3c314a67384492fe75
Oscar <oscarwang22396@gmail.com>
2021-09-22 09:14:20 -0400

    added text file for names

--- giveName.py
+++ giveName.py
@@ -1,70 +1,19 @@
 import random
 
-names ='''Patrick Ging
-Owen Yaggy
-Haotian Gan
-Wenhao Dong
-Ishraq Mahid
-Raymond Yeung
-Kevin Cao
-Ivan Lam
-Michelle Lo
-Christopher Liu
-Michael Borczuk
-Naomi Naranjo
-Ivan Mijacika
-Lucas Lee (Lewis Cass)
-Josephine Lee
-Rayat Roy
-Alif Abdullah
-William Chen
-Justin Zou
-Emma Buller
-Andy Lin
-Rachel Xiao
-Andrew Juang
-Eliza Knapp
-Yuqing Wu
-Renggeng Zheng
-Shadman Rakib
-AnnabelZhang1
-Alejandro Alonso
-Deven Maheshwari
-DavidASC20
-Liesel Wong
-Aryaman Goenka
-Oscar Wang
-Tami Takada
-Daniel Sooknanan
-Cameron Nelson
-Yusuf Elsharawy
-Austin Ngan
-Ella Krechmer
-Tomas Acuna
-Tina Nguyen
-Thomas Yu
-Yaying Liang Li
-Sadid Ethun
-Jesse Xie
-Eric Guo
-Jonathan Wu
-Shyne Choi
-Zhaoyu Lin
-Joshua Kloepfer
-Shriya Anand
-Noakai Aronesty
-Yoonah Chang
-Roshani Shrestha
-Theodore Fahey
-Qina Liu
-Han Zhang
-Lucas Tom-Wong
-Julia Nelson
-Sophie Liu
-Angela Zhang
-Gavin McGinley'''
+#opens the specified file and returns a list of each line's element
+def extract(fname):
+    f = open(fname)
+    s = f.read()
+    s = s.split('\n')
+    return s
 
-names = names.split('\n')
-names.sort()
-i = random.randint(0,len(names))
-print(names[i])
+#takes a list and prints a random element from the list
+def giveName(list):
+    print(list[random.randint(0,len(list))])
+
+#main function that executes on run
+def main():
+    giveName(extract('Names.txt'))
+
+if __name__ == '__main__':
+    main()
