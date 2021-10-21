077817466f957fc24d41630e66f5ef1bfbd33c72
Oscar <oscarwang22396@gmail.com>
2021-10-21 16:35:44 -0400

    added comments

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,7 +1,7 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev  
-#skeleton/stub :: SQLITE3 BASICS
-#Dec 2020 -- The Time of the Rona
+# Forgotten Charger: Lewis Cass, Aryaman Goenka, Oscar Wang
+# Softdev
+# K16: All About Database
+# 2021-10-21
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -14,10 +14,16 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 #==========================================================
 
-command = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER);"          # test SQL stmt in sqlite3 shell, save as string
+# creates the table for students.csv
+command = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER);"
 c.execute(command)    # run SQL statement
 
+# stores the contents of students.csv into the previously created table
 def getstudents(file):
+    '''
+    uses f strings to run sqlite commands that insert the data from each
+    row of the csv into the database
+    '''
     with open(file) as f:
         reader = csv.DictReader(f)
         for row in reader: 
@@ -28,12 +34,19 @@ def getstudents(file):
             command = f"INSERT INTO students VALUES ('{name}', {age}, {id});"
             c.execute(command)
 
+# calls the function
 getstudents("students.csv")
 
+# creates the table for courses.csv
 command = "CREATE TABLE courses (course TEXT, mark INTEGER, id INTEGER);"
 c.execute(command)
 
+# stores the contents of courses.csv into the previously created table
 def getcourses(file):
+    '''
+    uses f strings to run sqlite commands that insert the data from each
+    row of the csv into the database
+    '''
     with open(file) as f:
         reader = csv.DictReader(f)
         for row in reader: 
@@ -44,6 +57,7 @@ def getcourses(file):
             command = f"INSERT INTO courses VALUES ('{course}', {mark}, {id});"
             c.execute(command)
 
+# calls the function
 getcourses("courses.csv")
 
 #==========================================================
