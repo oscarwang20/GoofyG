329e7b90d013131b200411d5cde2725c20fbe7d2
Oscar <oscarwang22396@gmail.com>
2021-11-01 08:12:59 -0400

    a

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -2,6 +2,7 @@
 # Softdev
 # K16: All About Database
 # 2021-10-21
+# time spent: 45 minutes
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -15,8 +16,8 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 #==========================================================
 
 # creates the table for students.csv
-command = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER);"
-c.execute(command)    # run SQL statement
+# command = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER);"
+# c.execute(command)    # run SQL statement
 
 # stores the contents of students.csv into the previously created table
 def getstudents(file):
@@ -35,7 +36,7 @@ def getstudents(file):
             c.execute(command)
 
 # calls the function
-getstudents("students.csv")
+# getstudents("students.csv")
 
 # creates the table for courses.csv
 command = "CREATE TABLE courses (course TEXT, mark INTEGER, id INTEGER);"
@@ -53,8 +54,13 @@ def getcourses(file):
             course = row["code"]
             mark = int(row["mark"])
             id = int(row["id"])
+            try: 
+                test = row["test"]
+                print(test)
+                command = f"INSERT INTO courses VALUES ('{course}', {mark}, {id}, {test});"
+            except:
+                command = f"INSERT INTO courses (course, mark, id) VALUES ('{course}', {mark}, {id});"
             # print(course, mark, id)
-            command = f"INSERT INTO courses VALUES ('{course}', {mark}, {id});"
             c.execute(command)
 
 # calls the function
